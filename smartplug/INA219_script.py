9459f77684098056433872a57a270eedef971dd9
Jacob Tønder Hansen <93120233+Lfsjacob@users.noreply.github.com>
2024-03-24 15:59:58 +0100

    Update INA219_script.py

--- smartplug/INA219_script.py
+++ smartplug/INA219_script.py
@@ -9,12 +9,7 @@ def læs_ina():
     ina = INA219(SHUNT_OHMS, busnum=1, address=0x40)
     ina.configure()
     try:
-        # spænding = round(ina.voltage(), 3)
-        # strøm = abs(int(ina.current()))
         effekt = int(ina.power())
-        # print(f"Spænding: {spænding} V")
-        # print(f"Strøm: {strøm} mA")
-        # print(f"Effekt: {effekt} mW")
     except DeviceRangeError as e:
         print(e)
     return effekt
